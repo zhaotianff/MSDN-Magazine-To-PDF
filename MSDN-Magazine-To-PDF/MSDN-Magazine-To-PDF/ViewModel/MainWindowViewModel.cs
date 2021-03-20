5a59821052d87f5fd24526be86d255abff283c83
zhaotianff <zhaotianff@163.com>
2021-03-20 17:01:42 +0800

    build project framework

---
+++ MSDN-Magazine-To-PDF/MSDN-Magazine-To-PDF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDN_Magazine_To_PDF.ViewModel
+{
+    public class MainWindowViewModel
+    {
+
+    }
+}
