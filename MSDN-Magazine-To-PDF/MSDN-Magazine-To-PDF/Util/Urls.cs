74fbd28dc61a47d3fd5156a4ebb5f519e586ab21
zhaotianff <zhaotianff@163.com>
2021-03-21 21:41:19 +0800

    build project framework

---
+++ MSDN-Magazine-To-PDF/MSDN-Magazine-To-PDF/Util/Urls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDN_Magazine_To_PDF.Util
+{
+    public class Urls
+    {
+        public const string BaseUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/msdn-magazine-issues";
+    }
+}
