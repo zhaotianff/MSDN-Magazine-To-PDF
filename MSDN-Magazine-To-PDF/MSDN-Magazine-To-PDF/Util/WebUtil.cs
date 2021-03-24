1bb73a6d90923284de5c6c4feca1a96250104191
zhaotianff <zhaotianff@163.com>
2021-03-24 21:52:42 +0800

    httpclient

--- MSDN-Magazine-To-PDF/MSDN-Magazine-To-PDF/Util/WebUtil.cs
+++ MSDN-Magazine-To-PDF/MSDN-Magazine-To-PDF/Util/WebUtil.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 
 namespace MSDN_Magazine_To_PDF.Util
 {
     public class WebUtil
     {
-
+        public async static Task<string> GetHtml(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            return await httpClient.GetStringAsync(url);
+        }
     }
 }
