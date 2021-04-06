43c6a24cd927909c5d13b73d7bf2a339375d2193
zhaotianff <zhaotianff@163.com>
2021-04-06 21:40:58 +0800

    ui

--- MSDN-Magazine-To-PDF/MSDN-Magazine-To-PDF/ViewModels/MainWindowViewModel.cs
+++ MSDN-Magazine-To-PDF/MSDN-Magazine-To-PDF/ViewModels/MainWindowViewModel.cs
@@ -8,17 +8,26 @@ using Prism;
 using System.Collections.ObjectModel;
 using MSDN_Magazine_To_PDF.Model;
 using Prism.Mvvm;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace MSDN_Magazine_To_PDF.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private ObservableCollection<Magazine> magazineList = new ObservableCollection<Magazine>();
+        private ObservableCollection<System.Windows.Controls.Expander> monthList = new ObservableCollection<System.Windows.Controls.Expander>();
 
         public ObservableCollection<Magazine> MagazineList
         {
             get => magazineList;
             set => SetProperty(ref magazineList, value, "MagazineList");
+        }
+
+        public ObservableCollection<System.Windows.Controls.Expander> MonthList
+        {
+            get => monthList;
+            set => SetProperty(ref monthList, value, "MonthList");
         }
 
         public MainWindowViewModel()
@@ -27,6 +36,9 @@ namespace MSDN_Magazine_To_PDF.ViewModels
             MagazineList.Add(new Magazine() { Title = "Jan", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt848698.cover_lrg(en-us,msdn.10).jpg" });
             MagazineList.Add(new Magazine() { Title = "Feb", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt833264.cover_lg(en-us,msdn.10).png" });
             MagazineList.Add(new Magazine() { Title = "Mar", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt833283.cover_lrg(en-us,msdn.10).jpg" });
+
+            MonthList.Add(new System.Windows.Controls.Expander() { Header = "Test", Style = (System.Windows.Style)System.Windows.Application.Current.FindResource("ExpanderStyle") }) ;
+            MonthList.Add(new System.Windows.Controls.Expander() { Header = "Test2", Style = (System.Windows.Style)System.Windows.Application.Current.FindResource("ExpanderStyle") });
         }
     }
 }
