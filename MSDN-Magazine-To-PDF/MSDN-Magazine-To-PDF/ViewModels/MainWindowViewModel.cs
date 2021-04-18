1eb76ac9407bfcc2defa2ad6b6b5f3f985f66026
zhaotianff <zhaotianff@163.com>
2021-04-18 14:34:20 +0800

    get list

--- MSDN-Magazine-To-PDF/MSDN-Magazine-To-PDF/ViewModels/MainWindowViewModel.cs
+++ MSDN-Magazine-To-PDF/MSDN-Magazine-To-PDF/ViewModels/MainWindowViewModel.cs
@@ -10,13 +10,14 @@ using MSDN_Magazine_To_PDF.Model;
 using Prism.Mvvm;
 using System.Windows;
 using System.Windows.Controls;
+using MSDN_Magazine_To_PDF.Util;
 
 namespace MSDN_Magazine_To_PDF.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private ObservableCollection<Magazine> magazineList = new ObservableCollection<Magazine>();
-        private ObservableCollection<System.Windows.Controls.Expander> monthList = new ObservableCollection<System.Windows.Controls.Expander>();
+        private ObservableCollection<System.Windows.Controls.Expander> yearList = new ObservableCollection<System.Windows.Controls.Expander>();
 
         public ObservableCollection<Magazine> MagazineList
         {
@@ -24,21 +25,34 @@ namespace MSDN_Magazine_To_PDF.ViewModels
             set => SetProperty(ref magazineList, value, "MagazineList");
         }
 
-        public ObservableCollection<System.Windows.Controls.Expander> MonthList
+        public ObservableCollection<System.Windows.Controls.Expander> YearList
         {
-            get => monthList;
-            set => SetProperty(ref monthList, value, "MonthList");
+            get => yearList;
+            set => SetProperty(ref yearList, value, "YearList");
         }
 
         public MainWindowViewModel()
         {
-            //Test
-            MagazineList.Add(new Magazine() { Title = "Jan", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt848698.cover_lrg(en-us,msdn.10).jpg" });
-            MagazineList.Add(new Magazine() { Title = "Feb", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt833264.cover_lg(en-us,msdn.10).png" });
-            MagazineList.Add(new Magazine() { Title = "Mar", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt833283.cover_lrg(en-us,msdn.10).jpg" });
+            LoadMagazineList();
+        }
 
-            MonthList.Add(new System.Windows.Controls.Expander() { Header = "Test", Style = (System.Windows.Style)System.Windows.Application.Current.FindResource("ExpanderStyle") }) ;
-            MonthList.Add(new System.Windows.Controls.Expander() { Header = "Test2", Style = (System.Windows.Style)System.Windows.Application.Current.FindResource("ExpanderStyle") });
+        private async void LoadMagazineList()
+        {
+            var json = await WebUtil.GetHtml(Urls.GetListUrl);
+            var magazineRoot = JsonUtil.Deserialize<MagazineRoot>(json);
+
+            foreach (var item in magazineRoot.items.First()?.children)
+            {
+                YearList.Add(new Expander() { Header = item.toc_title,Height = 50, Style = (Style)Application.Current.FindResource("ExpanderStyle") });
+            }
+
+            ////Test
+            //MagazineList.Add(new Magazine() { Title = "Jan", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt848698.cover_lrg(en-us,msdn.10).jpg" });
+            //MagazineList.Add(new Magazine() { Title = "Feb", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt833264.cover_lg(en-us,msdn.10).png" });
+            //MagazineList.Add(new Magazine() { Title = "Mar", CoverUrl = "https://docs.microsoft.com/en-us/archive/msdn-magazine/images/mt833283.cover_lrg(en-us,msdn.10).jpg" });
+
+            
+            //YearList.Add(new System.Windows.Controls.Expander() { Header = "Test2", Style = (System.Windows.Style)System.Windows.Application.Current.FindResource("ExpanderStyle") });
         }
     }
 }
